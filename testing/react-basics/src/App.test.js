fa8383ded42efb714e6f962dd4fd5eefb79c0e6c
ryan.w.daly <ryan.w.daly90@gmail.com>
2020-05-10 19:30:59 -0400

    on to writing tests for the food lookup app

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import {shallow } from 'enzyme'
-// import ReactDOM from 'react-dom';
+/* leave first line blank for cq */
 import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
 
 describe('App', () => {
-  let wrapper; 
+  let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(
@@ -12,49 +12,7 @@ describe('App', () => {
     );
   });
 
-  describe('the user populates the input', () => {
-    const item = 'Vancouver';
-
-    beforeEach(() => {
-      const input = wrapper.find('input').first();
-      input.simulate('change', {
-        target: { value: item }
-      })
-    });
-
-    it('should update the state property `item`', () => {
-      expect(
-        wrapper.state().item
-      ).toEqual(item)
-    });
-
-    it('should enable `button`', () => {
-      const button = wrapper.find('button').first();
-      expect(
-        button.props().disabled
-      ).toBe(false);
-    });
-
-    describe('and clears the input feild', () => {
-      beforeEach(() => {
-        const input = wrapper.find('input').first();
-        input.simulate('change', {
-          target: { value: '' }
-        })
-      });
-      
-      it('should disable `button`', () => {
-        const button = wrapper.find('button').first();
-        expect(
-          button.props().disabled
-          ).toBe(true);
-      });
-    });
-  });
-
-
-
-  it('should have the `th` "items"', () => {
+  it('should have the `th` "Items"', () => {
     expect(
       wrapper.contains(<th>Items</th>)
     ).toBe(true);
@@ -68,14 +26,95 @@ describe('App', () => {
     ).toBe(true);
   });
 
-  it('button should be disabled', () => {
+  it('should have an `input` element', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <input />
+      )
+    ).toBe(true);
+  });
+
+  it('`button` should be disabled', () => {
     const button = wrapper.find('button').first();
     expect(
       button.props().disabled
     ).toBe(true);
   });
+
+  describe('the user populates the input', () => {
+    const item = 'Vancouver';
+
+    beforeEach(() => {
+      const input = wrapper.find('input').first();
+      input.simulate('change', {
+        target: { value: item }
+      });
+    });
+
+    it('should update the state property `item`', () => {
+      expect(
+        wrapper.state().item
+      ).toEqual(item);
+    });
+
+    it('should enable `button`', () => {
+      const button = wrapper.find('button').first();
+      expect(
+        button.props().disabled
+      ).toBe(false);
+    });
+
+    describe('and then clears the input', () => {
+      beforeEach(() => {
+        const input = wrapper.find('input').first();
+        input.simulate('change', {
+          target: { value: '' }
+        })
+      });
+
+      it('should disable `button`', () => {
+        const button = wrapper.find('button').first();
+        expect(
+          button.props().disabled
+        ).toBe(true);
+      });
+    });
+
+    describe('and then submits the form', () => {
+      beforeEach(() => {
+        const form = wrapper.find('form').first();
+        form.simulate('submit', {
+          preventDefault: () => {},
+        });
+      });
+
+      it('should add the item to state', () => {
+        expect(
+          wrapper.state().items
+        ).toContain(item);
+      });
+
+      it('should render the item in the table', () => {
+        expect(
+          wrapper.containsMatchingElement(
+            <td>{item}</td>
+          )
+        ).toBe(true);
+      });
+
+      it('should clear the input field', () => {
+        const input = wrapper.find('input').first();
+        expect(
+          input.props().value
+        ).toEqual('');
+      });
+
+      it('should disable `button`', () => {
+        const button = wrapper.find('button').first();
+        expect(
+          button.props().disabled
+        ).toBe(true);
+      });
+    });
+  })
 });
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
