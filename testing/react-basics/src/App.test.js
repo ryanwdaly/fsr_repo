12d4f25e274c2666101a55d2a7cb8a862a7626c3
ryan.w.daly <ryan.w.daly90@gmail.com>
2020-05-10 04:51:14 -0400

    up to form checks

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -4,12 +4,74 @@ import {shallow } from 'enzyme'
 import App from './App';
 
 describe('App', () => {
-  it('should have the `th` "items"', () => {
-    const wrapper = shallow(
+  let wrapper; 
+
+  beforeEach(() => {
+    wrapper = shallow(
       <App />
     );
+  });
+
+  describe('the user populates the input', () => {
+    const item = 'Vancouver';
+
+    beforeEach(() => {
+      const input = wrapper.find('input').first();
+      input.simulate('change', {
+        target: { value: item }
+      })
+    });
+
+    it('should update the state property `item`', () => {
+      expect(
+        wrapper.state().item
+      ).toEqual(item)
+    });
+
+    it('should enable `button`', () => {
+      const button = wrapper.find('button').first();
+      expect(
+        button.props().disabled
+      ).toBe(false);
+    });
+
+    describe('and clears the input feild', () => {
+      beforeEach(() => {
+        const input = wrapper.find('input').first();
+        input.simulate('change', {
+          target: { value: '' }
+        })
+      });
+      
+      it('should disable `button`', () => {
+        const button = wrapper.find('button').first();
+        expect(
+          button.props().disabled
+          ).toBe(true);
+      });
+    });
+  });
+
+
+
+  it('should have the `th` "items"', () => {
     expect(
       wrapper.contains(<th>Items</th>)
+    ).toBe(true);
+  });
+
+  it('should have a `button` element', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <button>Add item</button>
+      )
+    ).toBe(true);
+  });
+
+  it('button should be disabled', () => {
+    const button = wrapper.find('button').first();
+    expect(
+      button.props().disabled
     ).toBe(true);
   });
 });
